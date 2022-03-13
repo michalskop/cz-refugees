a83136a988bede4ef012dc55ecbb10ad2c0922b0
Michal Skop <michal.skop@kohovolit.eu>
2022-03-13 01:22:09 +0100

    praha

--- prepare_origin.py
+++ prepare_origin.py
@@ -13,9 +13,9 @@ orig_merged.columns
 
 orig_merged.to_csv(path + "origin.csv", index=False)
 
-fname = 'Strpeni-UKR_-_k_11-03-2022.xlsx'
-data = pd.read_excel(path + 'data/' + fname)
-values = data.columns[5:]
-pd.pivot_table(data, values=values, index=['obec', 'okres'], aggfunc=np.sum)
+# fname = 'Strpeni-UKR_-_k_11-03-2022.xlsx'
+# data = pd.read_excel(path + 'data/' + fname)
+# values = data.columns[5:]
+# pd.pivot_table(data, values=values, index=['obec', 'okres'], aggfunc=np.sum)
 
-orig_merged.merge(data, how="left", left_on=['district', 'name'], right_on=['okres', 'obec']).fillna(0)
+# orig_merged.merge(data, how="left", left_on=['district', 'name'], right_on=['okres', 'obec']).fillna(0)
