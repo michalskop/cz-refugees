09a4ccabedd2ed73c24922614768d585ef233e3b
Michal Skop <michal.skop@kohovolit.eu>
2022-04-29 13:58:42 +0200

    daily update

--- map0.py
+++ map0.py
@@ -9,7 +9,7 @@ origin = pd.read_csv('origin.csv')
 # today = datetime.datetime.today()
 # todate = today.strftime('%d-%m-%Y')
 
-fname = 'Statistika_UKR_28-04-2022.xlsx'
+fname = 'Statistika_UKR_29-04-2022.xlsx'
 
 today = (datetime.datetime.today())
 todate = today.strftime("%-d. %-m. %Y")
